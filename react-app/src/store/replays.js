7c03de1b24aa1bab05f9d80924dc411cbabf84e3
Sharon Fang <sharonfang8@gmail.com>
2022-04-18 20:16:25 -0700

    updated models and seeds, comments reducer and frontend form done, no css yet

--- react-app/src/store/replays.js
+++ react-app/src/store/replays.js
@@ -1,8 +1,20 @@
 const GET_GAME = "games/GET_GAME";
+const GET_COMMENT = "games/GET_COMMENT";
+const DELETE_COMMENT = "games/DELETE_COMMENT";
 
 const loadGame = (game) => ({
   type: GET_GAME,
   game,
+});
+
+const loadComment = (comment) => ({
+  type: GET_COMMENT,
+  comment,
+});
+
+const removeComment = (comment_id) => ({
+  type: DELETE_COMMENT,
+  comment_id,
 });
 
 export const fetchGame = (game_id) => async (dispatch) => {
@@ -19,27 +31,86 @@ export const fetchGame = (game_id) => async (dispatch) => {
   }
 };
 
+export const editGame = (privateData) => async (dispatch) => {
+  const res = await fetch(`/api/games/${privateData.id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(privateData),
+  });
+
+  if (!res.ok) {
+    return res.errors;
+  }
+  const updatedGame = await res.json();
+  console.log("thunk privateData", privateData);
+  console.log("thunk updatedGame", updatedGame);
+
+  dispatch(loadGame(updatedGame));
+  return updatedGame;
+};
+
 export const saveGame = (payload) => async (dispatch) => {
   const res = await fetch(`/api/games/`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload),
-    /**
-     *player_one_id = data['player_one_id'],
-      player_two_id = data['player_one_id'],
-      winner_id = data['winner_id'],
-      moves = data['moves'],
-     */
-  })
+  });
 
   if (res.ok) {
     const data = await res.json();
-    dispatch(loadGame(data))
+    dispatch(loadGame(data));
   } else {
     const errors = await res.json();
     return errors;
   }
-}
+};
+
+export const addComment = (payload) => async (dispatch) => {
+  const res = await fetch(`/api/comments/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(loadComment(data));
+  } else {
+    const errors = await res.json();
+    return errors;
+  }
+};
+
+export const editComment = (payload) => async (dispatch) => {
+  const res = await fetch(`/api/comments/${payload.comment_id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+
+  if (res.ok) {
+    const data = await res.json(); //returns comment obj
+    dispatch(loadComment(data));
+  } else {
+    const errors = await res.json();
+    return errors;
+  }
+};
+
+export const deleteComment = (comment_id) => async (dispatch) => {
+  const res = await fetch(`/api/comments/${comment_id}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (res.ok) {
+    const data = await res.json(); //return comment id
+    dispatch(removeComment(data));
+  } else {
+    const errors = await res.json();
+    return errors;
+  }
+};
 
 let initialState = {};
 
@@ -49,6 +120,11 @@ const replaysReducer = (state = initialState, action) => {
     case GET_GAME:
       newState = action.game;
       return newState;
+    case GET_COMMENT:
+      newState.comments[action.comment.id] = action.comment;
+      return newState;
+    case DELETE_COMMENT:
+      delete newState.comments[action.comment_id];
     default:
       return state;
   }
